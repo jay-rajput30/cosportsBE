1fc2b946d6ed36df6a175d5629d89e6931b7ed22
Jay Rajput <74800982+jay-rajput30@users.noreply.github.com>
2021-12-29 04:55:38 +0530

    feat: added edit user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -25,4 +25,55 @@ const addUser = async (req, res) => {
   }
 };
 
-module.exports = { addUser };
+const findSingleUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userFound = await User.findById(id);
+    console.log({ userFound });
+    res.status(200).json({ success: true, user: userFound });
+  } catch (err) {
+    console.log({ err });
+    res.status(503).json({ success: false, err });
+  }
+};
+
+const editExistingUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { firstName, lastName, email, username } = req.body;
+    console.log({ firstName, lastName, email, username, id });
+    const existingEmail = await User.findOne({ email });
+    const existingUsername = await User.findOne({ username });
+
+    if (existingEmail) {
+      res.status(503).json({ success: false, message: "email already exists" });
+    } else if (existingUsername) {
+      res
+        .status(503)
+        .json({ success: false, message: "username already exists" });
+    } else {
+      const userFound = await User.findById(id);
+      userFound.firstName = firstName;
+      userFound.lastName = lastName;
+      userFound.email = email;
+      userFound.username = username;
+
+      await userFound.save();
+
+      res.status(200).json({ success: true, user: userFound });
+    }
+  } catch (err) {
+    console.log({ err });
+    res.status(503).json({ success: false, err });
+  }
+};
+
+{
+    firstName:"Jay",
+    lastName:"Rajput",
+    email:"jayrajput@gmail.com",
+    password: "jayrajput",
+    username:"jayrajput30"
+}
+
+module.exports = { addUser, findSingleUser, editExistingUser };
