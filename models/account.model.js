d0c5ab56b7d0746c6ac1a5cc08b9429861b7bada
Jay Rajput <74800982+jay-rajput30@users.noreply.github.com>
2021-12-29 05:41:12 +0530

    feat: created account model

--- models/account.model.js
+++ models/account.model.js
@@ -1,1 +1,33 @@
- 
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const accountSchema = Schema(
+  {
+    uid: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    following: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    followers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    bio: {
+      type: String,
+      required: "the users bio is required",
+      default: "Hey there. How you doin?",
+    },
+  },
+  { timestamps: true }
+);
+
+const Account = mongoose.model("account", accountSchema);
+
+module.exports = Account;
