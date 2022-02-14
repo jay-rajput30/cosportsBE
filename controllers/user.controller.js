0bf90c3c3e2af9d0fceba2381305f8f548d4bc8c
Jay Rajput <74800982+jay-rajput30@users.noreply.github.com>
2022-02-14 16:58:50 +0530

    refactor: changed user sign up controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/user.model");
 const Account = require("../models/account.model");
-
+const jwt = require("jsonwebtoken");
 const getAllUsers = async (req, res) => {
   try {
     const allUsers = await Account.find({}).populate("uid");
@@ -17,47 +17,75 @@ const getAllUsers = async (req, res) => {
 
 const addUser = async (req, res) => {
   try {
-    const {
-      firstName,
-      lastName,
-      location,
-      website,
-      email,
-      username,
-      password,
-    } = req.body;
-    bcrypt.genSalt(10, async (err, salt) => {
-      bcrypt.hash(password, salt, async (err, hash) => {
-        const user = new User({
-          firstName,
-          lastName,
-          location,
-          website,
-          email,
-          username,
-          password: hash,
-        });
-        await user.save();
+    const { formData } = req.body;
 
-        const account = new Account({
-          uid: user._id,
-          following: [],
-          followers: [],
-          bio: "how you doin?",
-        });
+    const allUsers = await User.find({});
+    console.log({
+      allUsers,
+      formData,
+      username: formData.username,
+    });
+    console.log(formData.username);
+    const usernameAlreadyExists = allUsers.findIndex((user) => {
+      console.log({ username: user.username, reqUsername: formData.username });
+      return user.username.toString() == formData.username.toString();
+    });
+    const emailAlreadyExists = allUsers.findIndex(
+      (user) => user.email.toString() === formData.email.toString()
+    );
+    console.log({ usernameAlreadyExists, emailAlreadyExists });
+    if (usernameAlreadyExists === -1 && emailAlreadyExists === -1) {
+      bcrypt.genSalt(10, async (err, salt) => {
+        bcrypt.hash(formData.password, salt, async (err, hash) => {
+          const user = new User({
+            firstName: formData.firstName,
+            lastName: formData.lastName,
+            location: formData.location,
+            website: formData.website,
+            email: formData.email,
+            username: formData.username,
+            password: hash,
+          });
+          await user.save();
 
-        await account.save();
-        res.status(200).json({
-          firstName,
-          lastName,
-          location,
-          website,
-          email,
-          username,
-          password: hash,
+          const account = new Account({
+            uid: user._id,
+            following: [],
+            followers: [],
+            bio: "",
+          });
+
+          await account.save();
+
+          const userDetails = {
+            userId: user._id,
+            username: user.username,
+            email: user.email,
+          };
+          const token = jwt.sign(userDetails, process.env.MY_SECRET_KEY, {
+            expiresIn: "12000000ms",
+          });
+
+          user.password = undefined;
+
+          res.status(200).json({
+            success: true,
+            user,
+            token,
+          });
         });
       });
-    });
+    } else if (usernameAlreadyExists !== -1) {
+      res.status(503).json({
+        success: false,
+        err: "username  already exists. try different one",
+      });
+    } else if (emailAlreadyExists !== -1) {
+      res.status(503).json({
+        success: false,
+        err: "email  already exists. try different one",
+      });
+    }
   } catch (err) {
     console.log({ err });
     res.status(503).json({ success: false, err });
