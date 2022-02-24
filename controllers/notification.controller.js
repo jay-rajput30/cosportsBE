77df619b16171edf8528562dfa9132501a64bbd2
Jay Rajput <74800982+jay-rajput30@users.noreply.github.com>
2022-02-24 19:10:27 +0530

    refactor: added notification controller

---
+++ controllers/notification.controller.js
@@ -0,0 +1,32 @@
+const Notification = require("../models/notification.model");
+
+const getAllNotifications = async (req, res) => {
+  try {
+    const allNotifications = await Notification.find({})
+      .sort({ createdAt: 1 })
+      .limit(15);
+
+    console.log({ allNotifications });
+    res.status(200).json({ success: true, notifications: allNotifications });
+  } catch (e) {
+    res.status(503).json({ success: false, error: e });
+    console.error({ error: e });
+  }
+};
+
+const removeNotification = async (req, res) => {
+  try {
+    const { postId } = req.body;
+    const allNotifications = await Notification.find({});
+
+    allNotifications.filter((notification) => notification.postId === postId);
+    await allNotifications.save();
+
+    res.status(200).json({ success: true });
+  } catch (e) {
+    res.status(503).json({ success: false, error: e });
+    console.error({ error: e });
+  }
+};
+
+module.exports = { getAllNotifications, removeNotification };
--- routes/notification.route.js
+++ routes/notification.route.js
@@ -1,22 +1,16 @@
 const express = require("express");
 
 const router = express.Router();
-const Notification = require("../models/notification.model");
 
 const authenticateRoute = require("../middlewares/route.auth");
 
-router.get("/", authenticateRoute, async (req, res) => {
-  try {
-    const allNotifications = await Notification.find({})
-      .sort({ createdAt: 1 })
-      .limit(15);
+const {
+  getAllNotifications,
+  removeNotification,
+} = require("../controllers/notification.controller");
 
-    console.log({ allNotifications });
-    res.status(200).json({ success: true, notifications: allNotifications });
-  } catch (e) {
-    res.status(503).json({ success: false, error: e });
-    console.error({ error: e });
-  }
-});
+router.get("/", authenticateRoute, getAllNotifications);
+
+router.post("/remove", authenticateRoute, removeNotification);
 
 module.exports = router;
